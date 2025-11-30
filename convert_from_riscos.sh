36462b4effea4c751aba69ba8deea92213425c52
Rob McKay <16419309+Rob-McKay@users.noreply.github.com>
2025-11-30 19:04:24 +0000

    Fixed some porting issues

--- convert_from_riscos.sh
+++ convert_from_riscos.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Convert any changed files in the RISC OS source tree back into the standard libsmb2 source tree
 #
 # Copyright 2023 Rob McKay
@@ -19,15 +19,11 @@
 
 
 function copy_if_different() {
-    if [ -f $1 ]; then
-        diff --strip-trailing-cr -q -N $1 $2
+    if [ -f "$1" ]; then
+        diff --strip-trailing-cr -q -N "$1" "$2"
         if [ $? -eq 1 ]; then
             echo "Updating '$2' from '$1'"
-            chmod u+rw $1
-            if [ -f $2 ]; then
-                chmod u+rw $2
-            fi
-            cp -pv $1 $2
+            cp -pv "$1" "$2"
         fi
     fi
 }
@@ -44,40 +40,46 @@ done
 
 # lib/*.(c|h) files
 
-for source in 'aes' 'aes128ccm' 'alloc' 'compat' 'dcerpc' 'dcerpc-lsa' 'dcerpc-srvsvc' 'errors' 'hmac' 'hmac-md5' 'init' 'krb5-wrapper' 'libsmb2' 'md4' 'md4c' 'md5' 'ntlmssp' 'pdu' 'sha' 'sha-private' 'sha1' 'sha224-256' 'sha384-512' 'smb2-cmd-close' 'smb2-cmd-create' 'smb2-cmd-echo' 'smb2-cmd-error' 'smb2-cmd-flush' 'smb2-cmd-ioctl' 'smb2-cmd-logoff' 'smb2-cmd-negotiate' 'smb2-cmd-query-directory' 'smb2-cmd-query-info' 'smb2-cmd-read' 'smb2-cmd-session-setup' 'smb2-cmd-set-info' 'smb2-cmd-tree-connect' 'smb2-cmd-tree-disconnect' 'smb2-cmd-write' 'smb2-data-file-info' 'smb2-data-filesystem-info' 'smb2-data-reparse-point' 'smb2-data-security-descriptor' 'smb2-share-enum' 'smb2-signing' 'smb3-seal' 'socket' 'sync' 'timestamps' 'unicode' 'usha'; do
+for source in 'aes' 'aes_apple' 'aes_reference' 'aes128ccm' 'alloc' 'asn1-ber' 'compat' 'dcerpc' 'dcerpc-lsa' 'dcerpc-srvsvc' 'errors' 'hmac' 'hmac-md5' \
+                'init' 'krb5-wrapper' 'libsmb2' \
+                'md4' 'md4c' 'md5' 'ntlmssp' 'pdu' 'sha' 'sha-private' 'sha1' 'sha224-256' 'sha384-512' 'smb2-cmd-close' 'smb2-cmd-create' 'smb2-cmd-echo' \
+                'smb2-cmd-error' 'smb2-cmd-flush' 'smb2-cmd-ioctl' 'smb2-cmd-logoff' 'smb2-cmd-negotiate' 'smb2-cmd-query-directory' 'smb2-cmd-query-info' \
+                'smb2-cmd-read' 'smb2-cmd-session-setup' 'smb2-cmd-set-info' 'smb2-cmd-tree-connect' 'smb2-cmd-tree-disconnect' 'smb2-cmd-write' \
+                'smb2-data-file-info' 'smb2-data-filesystem-info' 'smb2-data-reparse-point' 'smb2-data-security-descriptor' 'smb2-share-enum' 'smb2-signing' \
+                'smb3-seal' 'socket' 'spnego-wrapper' 'sync' 'timestamps' 'unicode' 'usha'; do
     for type in 'c' 'h'; do
-        copy_if_different library/lib/$type/$source libsmb2/lib/$source.$type
+        copy_if_different "library/lib/$type/$source" "libsmb2/lib/$source.$type"
     done
 done
 
 # include/smb2/*.h files
 
 for header in 'libsmb2-dcerpc' 'libsmb2-dcerpc-lsa' 'libsmb2-dcerpc-srvsvc' 'libsmb2' 'libsmb2-raw' 'smb2-errors' 'smb2'; do
-    copy_if_different library/include/smb2/h/$header libsmb2/include/smb2/$header.h
+    copy_if_different "library/include/smb2/h/$header" "libsmb2/include/smb2/$header.h"
 done
 
 # include/*.h files
 
 for header in 'libsmb2-private' 'portable-endian' 'slist'; do
-    copy_if_different library/include/h/$header libsmb2/include/$header.h
+    copy_if_different "library/include/h/$header" "libsmb2/include/$header.h"
 done
 
 # RISC OS specific header files
 
 for header in 'config'; do
-    copy_if_different library/h/$header libsmb2/riscos/$header.h
+    copy_if_different "library/h/$header" "libsmb2/riscos/$header.h"
 done
 
 # RISC OS specific source files
 
 for source in 'asprintf'; do
     for type in 'c' 'h'; do
-        copy_if_different library/$type/$source libsmb2/riscos/$source.$type
+        copy_if_different "library/$type/$source" "libsmb2/riscos/$source.$type"
     done
 done
 
 # RISC OS specific files which should live in the standard libsmb2 source tree
 
 for file in 'Makefile.riscos' 'Mk,fd7' 'MkClean,fd7' ; do
-    copy_if_different library/$file libsmb2/riscos/$file
+    copy_if_different "library/$file" "libsmb2/riscos/$file"
 done
--- convert_to_riscos.sh
+++ convert_to_riscos.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # Convert the standard libsmb2 source tree into the RISC OS source tree.
 #
@@ -25,11 +25,11 @@
 
 
 function copy_or_diff() {
-    if [ -f $1 ]; then
-        if [ ! -f $2 ]; then
-            cp -n -p $1 $2
+    if [ -f "$1" ]; then
+        if [ ! -f "$2" ]; then
+            cp -n -p "$1" "$2"
         else
-            diff --strip-trailing-cr -p $1 $2
+            diff --strip-trailing-cr -p "$1" "$2"
         fi
     fi
 }
@@ -38,55 +38,54 @@ function copy_or_diff() {
 # Ensure that the required directories exist, and are read and writable from RISC OS
 
 for dir in 'c' 'h' 'o' 'lib' 'lib/h' 'lib/c' 'include/h' 'include/smb2/h'; do
-    if [ ! -d $dir ]; then
-        mkdir -p library/$dir
-        chmod u=rwx,g=rwx,o=rwx library/$dir
+    if [ ! -d "library/$dir" ]; then
+        mkdir -p "library/$dir"
     fi
 done
-chmod u=rwx,g=rwx,o=rwx library
 
 # lib/*.(c|h) files
 
 for source in 'aes' 'aes_apple' 'aes_reference' 'aes128ccm' 'alloc' 'asn1-ber' 'compat' 'dcerpc' 'dcerpc-lsa' 'dcerpc-srvsvc' 'errors' 'hmac' 'hmac-md5' \
                 'init' 'krb5-wrapper' 'libsmb2' \
                 'md4' 'md4c' 'md5' 'ntlmssp' 'pdu' 'sha' 'sha-private' 'sha1' 'sha224-256' 'sha384-512' 'smb2-cmd-close' 'smb2-cmd-create' 'smb2-cmd-echo' \
-                'smb2-cmd-error' 'smb2-cmd-flush' 'smb2-cmd-ioctl' 'smb2-cmd-logoff' 'smb2-cmd-negotiate' 'smb2-cmd-query-directory' 'smb2-cmd-query-info' \
+                'smb2-cmd-error' 'smb2-cmd-flush' 'smb2-cmd-ioctl' 'smb2-cmd-lock' 'smb2-cmd-logoff' 'smb2-cmd-negotiate' 'smb2-cmd-notify-change' 'smb2-cmd-oplock-break' \
+                'smb2-cmd-query-directory' 'smb2-cmd-query-info' \
                 'smb2-cmd-read' 'smb2-cmd-session-setup' 'smb2-cmd-set-info' 'smb2-cmd-tree-connect' 'smb2-cmd-tree-disconnect' 'smb2-cmd-write' \
                 'smb2-data-file-info' 'smb2-data-filesystem-info' 'smb2-data-reparse-point' 'smb2-data-security-descriptor' 'smb2-share-enum' 'smb2-signing' \
                 'smb3-seal' 'socket' 'spnego-wrapper' 'sync' 'timestamps' 'unicode' 'usha'; do
     for type in 'c' 'h'; do
-        copy_or_diff libsmb2/lib/$source.$type  library/lib/$type/$source
+        copy_or_diff "libsmb2/lib/$source.$type" "library/lib/$type/$source"
     done
 done
 
 # include/smb2/*.h files
 
 for header in 'libsmb2-dcerpc' 'libsmb2-dcerpc-lsa' 'libsmb2-dcerpc-srvsvc' 'libsmb2' 'libsmb2-raw' 'smb2-errors' 'smb2'; do
-    copy_or_diff libsmb2/include/smb2/$header.h library/include/smb2/h/$header
+    copy_or_diff "libsmb2/include/smb2/$header.h" "library/include/smb2/h/$header"
 done
 
 # include/*.h files
 
 for header in 'libsmb2-private' 'portable-endian' 'slist'; do
-    copy_or_diff libsmb2/include/$header.h library/include/h/$header
+    copy_or_diff "libsmb2/include/$header.h" "library/include/h/$header"
 done
 
 # RISC OS specific header files
 
 for header in 'config'; do
-    copy_or_diff libsmb2/riscos/$header.h library/h/$header
+    copy_or_diff "libsmb2/riscos/$header.h" "library/h/$header"
 done
 
 # RISC OS specific source files
 
 for source in 'asprintf'; do
     for type in 'c' 'h'; do
-        copy_or_diff libsmb2/riscos/$source.$type library/$type/$source
+        copy_or_diff "libsmb2/riscos/$source.$type" "library/$type/$source"
     done
 done
 
 # RISC OS specific files
 
 for file in 'Makefile.riscos' 'Mk,fd7' 'MkClean,fd7'; do
-    copy_or_diff libsmb2/riscos/$file library/$file
+    copy_or_diff "libsmb2/riscos/$file" "library/$file"
 done
